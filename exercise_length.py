fb42db1e920cf3663b73a6301f2f6f319be1240f
Thebe Nkhasi <thebzennkhasi@gmail.com>
2025-03-20 10:26:14 +0200

    Delete exercise_length.py

--- exercise_length.py
+++
@@ -1,7 +0,0 @@
-def get_length(input_string):
-    return len(input_string)
-
-student_name = input("Please enter student name: ")
-
-length = get_length(student_name)
-print(f"The length of the student name is: {length}")
